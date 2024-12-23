5d1fb6b912c8cd7780a8d984cc654ee2d516d3bd
itsathefak <athefayubkhan@gmail.com>
2024-12-23 14:28:21 -0500

    Structuring Components

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 import Title from "./Title.jsx"
 import { Button } from './Button.jsx'
 import Description from "./Description.jsx"
+import ProductTab from "./ProductTab.jsx"
+
 
 function App() {
   return(
     <>
       <Title></Title>
       <Description/>
+      <ProductTab/>
       <Button/>
     </>
   )
---
+++ basic-react-app/src/Product.jsx
@@ -0,0 +1,10 @@
+function Product(){
+    return(
+        <>
+        <h3>Your Products</h3>
+        <p>Description of your Product</p>
+        </>
+    )
+}
+
+export default Product;
