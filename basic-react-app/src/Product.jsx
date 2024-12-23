32791b701b02ee7e7871caf18a2baaa3f41372a5
itsathefak <athefayubkhan@gmail.com>
2024-12-23 14:40:23 -0500

    Add Styling to Components

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,9 +1,11 @@
+import "./Product.css"
+
 function Product(){
     return(
-        <>
+        <div className="Product">
         <h3>Your Products</h3>
         <p>Description of your Product</p>
-        </>
+        </div>
     )
 }
 
