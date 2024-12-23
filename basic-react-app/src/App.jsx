f325b2bdbce61f966ee4c8894ad8e5a3f97bfffd
itsathefak <athefayubkhan@gmail.com>
2024-12-23 13:32:21 -0500

    Creating my First Component

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,7 +1,24 @@
 import './App.css'
 
+function Title(){
+  return(
+    <h1>Welcome to my React App</h1>
+  )
+}
+
+function Button(){
+  return(
+    <button>Hello World</button>
+  )
+}
+
 function App() {
-  return <button>Hello World</button>
+  return(
+    <div>
+      <Title></Title>
+      <Button/>
+    </div>
+  )
 }
 
 export default App
