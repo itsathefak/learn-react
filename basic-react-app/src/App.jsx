36e5a542633c81c31dbe896bd0fd40badc784aa5
itsathefak <athefayubkhan@gmail.com>
2024-12-23 14:12:37 -0500

    Using {} for embedding pure JS

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Title from "./Title.jsx"
 import { Button } from './Button.jsx'
+import Description from "./Description.jsx"
 
 function App() {
   return(
     <>
       <Title></Title>
+      <Description/>
       <Button/>
     </>
   )
---
+++ basic-react-app/src/Description.jsx
@@ -0,0 +1,10 @@
+function Description(){
+    let name = "Athef"
+    return(
+        <>
+      <p>Welcome back {name}</p>  {/* Using {} for embedding pure JS */}
+      </>
+    )
+  }
+
+  export default Description;
