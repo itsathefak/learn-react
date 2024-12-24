0466e629ded5e4dcc17b9393a3f003d51f210c29
itsathefak <athefayubkhan@gmail.com>
2024-12-24 12:28:40 -0500

    Handling Non Click Events

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Title from "./Title.jsx"
-import { Button } from './Button.jsx'
+import Button from './Button.jsx'
 import Description from "./Description.jsx"
 import ProductTab from "./ProductTab.jsx"
 import MsgBox from "./MsgBox.jsx"
--- basic-react-app/src/Button.jsx
+++ basic-react-app/src/Button.jsx
@@ -1,11 +1,24 @@
-function printHello(){
+function handleOnClick(){
   console.log("Hello !")
+}
+
+function handleMouseOver(){
+  console.log("Bye !")
+}
+
+function handleDblClick(){
+  console.log("I juts got Double Clicked!")
 }
 
 function Button(){
     return(
-      <button onClick={printHello}>Buy Now</button>
+      <div>
+      <button onClick={handleOnClick}>Buy Now</button>
+      <p onMouseOver={handleMouseOver}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsam mollitia repellat beatae molestiae exercitationem ut deserunt quo eum. Nobis dolores aspernatur voluptatem hic? Quo recusandae officia voluptates rerum similique.
+      </p>
+      <button onDoubleClick={handleDblClick}>Double Click Me !</button>
+      </div>
     )
   }
 
-  export {Button};
+  export default Button;
