55f98495ebf5b05882f200c1f3e1a61e33415864
itsathefak <athefayubkhan@gmail.com>
2024-12-23 21:13:24 -0500

    Rendering Array

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,13 +1,14 @@
 import "./Product.css"
 
 function Product({title, price=1, features, options}){ //Passing Default Value as 1
+    // let list = features.map((feature)=> <li>{feature}</li>)
     return(
         <div className="Product">
         <h3>{title}</h3>
-        <p>Price = {price}</p>
-        <p>Features = {features}</p>
-        <p>Storage = {options.Storage}</p>
-        <p>Colour = {options.Colour}</p>
+        <p>Price : {price}</p>
+        <p>{features.map((feature)=> <li>{feature}</li>)}</p>
+        <p>Storage : {options.Storage}</p>
+        <p>Colour : {options.Colour}</p>
         </div>
     )
 }
--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -1,10 +1,11 @@
 import Product from "./Product"
 
 function ProductTab(){
+    let features = ["Fast", "Durable", "Hi-Speed"];
     return(
         <>
         {/* Passing Props for populating the component*/}
-        <Product title="Phone" price={30000} features={["Fast", "Durable", "Hi-Speed"]} options={{Storage:"512gb", Colour:"White"}}/>  
+        <Product title="Phone" price={30000} features={features} options={{Storage:"512gb", Colour:"White"}}/>  
         <Product title="Laptop" price={40000}features={["Fast", "Durable", "Hi-Speed"]} options={{Storage:"512gb", Colour:"White"}}/>
         <Product title="Watch" price={20000}features={["Fast", "Durable", "Hi-Speed"]} options={{Storage:"512gb", Colour:"White"}}/>
         </>
