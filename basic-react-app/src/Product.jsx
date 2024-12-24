a94a5707ba19adaa55ef66c52fa4070e0b0302f1
itsathefak <athefayubkhan@gmail.com>
2024-12-23 20:54:14 -0500

    Passing Arrays and objects as props

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,10 +1,13 @@
 import "./Product.css"
 
-function Product({title, price=1}){ //Passing Default Value as 1
+function Product({title, price=1, features, options}){ //Passing Default Value as 1
     return(
         <div className="Product">
         <h3>{title}</h3>
         <p>Price = {price}</p>
+        <p>Features = {features}</p>
+        <p>Storage = {options.Storage}</p>
+        <p>Colour = {options.Colour}</p>
         </div>
     )
 }
--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -4,9 +4,9 @@ function ProductTab(){
     return(
         <>
         {/* Passing Props for populating the component*/}
-        <Product title="Phone" price={30000}/>  
-        <Product title="Laptop" price={40000}/>
-        <Product title="Watch" price={20000}/>
+        <Product title="Phone" price={30000} features={["Fast", "Durable", "Hi-Speed"]} options={{Storage:"512gb", Colour:"White"}}/>  
+        <Product title="Laptop" price={40000}features={["Fast", "Durable", "Hi-Speed"]} options={{Storage:"512gb", Colour:"White"}}/>
+        <Product title="Watch" price={20000}features={["Fast", "Durable", "Hi-Speed"]} options={{Storage:"512gb", Colour:"White"}}/>
         </>
     )
 }
