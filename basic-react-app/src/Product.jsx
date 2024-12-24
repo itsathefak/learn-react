d0c607e3f73ac4931ab2d66a9338e5c08754d644
itsathefak <athefayubkhan@gmail.com>
2024-12-23 21:31:50 -0500

    Using Conditionals

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -6,6 +6,8 @@ function Product({title, price=1, features, options}){ //Passing Default Value a
         <div className="Product">
         <h3>{title}</h3>
         <p>Price : {price}</p>
+        {/* {price > 30000 ? <p>Discount of 5%</p> : null}         Same work, Different method */}
+        {price > 30000 && <p>Discount of 5%</p>}
         <p>{features.map((feature)=> <li>{feature}</li>)}</p>
         <p>Storage : {options.Storage}</p>
         <p>Colour : {options.Colour}</p>
