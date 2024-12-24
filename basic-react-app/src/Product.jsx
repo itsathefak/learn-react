e360b5485a908e470aa7c875b662e10197414156
itsathefak <athefayubkhan@gmail.com>
2024-12-23 21:44:24 -0500

    Dynamic Component Styling

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -2,12 +2,14 @@ import "./Product.css"
 
 function Product({title, price=1, features, options}){ //Passing Default Value as 1
     // let list = features.map((feature)=> <li>{feature}</li>)
+    let isDiscount = price > 30000;
+    let styles = {backgroundColor : isDiscount ? "lightblue" : null}
     return(
-        <div className="Product">
+        <div className="Product" style={styles}>
         <h3>{title}</h3>
         <p>Price : {price}</p>
         {/* {price > 30000 ? <p>Discount of 5%</p> : null}         Same work, Different method */}
-        {price > 30000 && <p>Discount of 5%</p>}
+        { isDiscount && <p>Discount of 5%</p>}
         <p>{features.map((feature)=> <li>{feature}</li>)}</p>
         <p>Storage : {options.Storage}</p>
         <p>Colour : {options.Colour}</p>
