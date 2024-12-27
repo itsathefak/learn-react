bcd2f30f0efe5dbe74628e8d0e205f831a654672
itsathefak <athefayubkhan@gmail.com>
2024-12-27 16:11:45 -0500

    Use Effect with dependency

--- basic-react-app/src/Counter.jsx
+++ basic-react-app/src/Counter.jsx
@@ -1,19 +1,28 @@
 import { useState, useEffect } from "react"
 
 export default function Counter(){
-    let[count, setCount] = useState(0);
+    let[countx, setCountx] = useState(0);
+    let[county, setCounty] = useState(0);
 
-    let incCount = ()=>{
-        setCount(currCount => currCount+1);
+    let incCountx = ()=>{
+        setCountx(currCount => currCount+1);
     }
+
+    let incCounty = ()=>{
+        setCounty(currCount => currCount+1);
+    }
+
 
     useEffect(function sideEffect(){
         console.log("This is a Side Effect")
-    })
+    }, [countx])
     return(
         <div>
-            <h3>Count = {count}</h3>
-            <button onClick={incCount}>+1</button>
+            <h3>Countx = {countx}</h3>
+            <button onClick={incCountx}>+1</button>
+
+            <h3>County = {county}</h3>
+            <button onClick={incCounty}>+1</button>
         </div>
     )
 }
